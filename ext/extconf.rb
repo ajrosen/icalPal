dad40553a84bbfeb4f193a0590fd0ef9c70a1abf
Andy Rosen <ajr@corp.mlfs.org>
2026-01-01 18:07:56 -0500

    Tailor dependencies to Ruby version
    Tested with 2.6.10p210 (macOS), 3.4.8 (homebrew), 4.0.0 (homebrew)

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -9,31 +9,16 @@ begin
   # Dependencies common to all environments
   dependencies = %w[ plist ]
 
-  if RUBY_VERSION >= '3.4'
-    # bigdecimal is not part of the default gems starting from Ruby 3.4.0.
-    # csv is not part of the default gems starting from Ruby 3.4.0.
-    dependencies.push('bigdecimal')
+  # All dependencies are included with the installation of Ruby in
+  # macOS. Adding these dependencies anyway will cause errors that
+  # prevent icalPal from being installed.
+  unless RUBY_VERSION >= '2.6'
+    dependencies.push('logger')
     dependencies.push('csv')
-
-    # The macOS and Homebrew versions of rubygems have incompatible
-    # requirements for sqlite3.
-    #
-    # macOS 15.5 (Sequoia) comes with version 1.3.13, so it does not
-    # need to be added as a dependency, and it cannot install anything
-    # newer:
-    #
-    # requires Ruby version >= 3.0, < 3.4.dev. The current ruby version is 2.6.10.
-    #
-    # Homebrew's Ruby formula does not come with sqlite3, so it does
-    # need to be added as a dependency, but it cannot install version
-    # 1.3.13:
-    #
-    # error: call to undeclared function
-    #
-    # So neither environment can install the other's sqlite3 gem.  We
-    # must install sqlite3, but iff we are not building with macOS'
-    # Ruby installation.
+    dependencies.push('json')
+    dependencies.push('rdoc')
     dependencies.push('sqlite3')
+    dependencies.push('yaml')
   end
 
   di = Gem::DependencyInstaller.new(install_dir: gemdir)
