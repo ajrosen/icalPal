f7b52b64b9a768e8c08d79e3895fea6f394a3aa6
Andy Rosen <ajr@corp.mlfs.org>
2026-05-02 17:32:04 -0400

    Simplify installation of dependencies (#54)

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,34 +1,19 @@
 begin
-  require 'mkmf'
   require 'rubygems/dependency_installer'
 
-  # Search Gem paths for one that is writable.
-  gemdir = nil
-  Gem.path.each { |p| gemdir = p if File.writable? p }
+  # Install dependencies that are not already installed
+  stubs = Gem::Specification.stubs.map(&:name)
 
-  # Dependencies common to all environments
-  dependencies = %w[ plist ]
+  dependencies = %w[ plist logger csv json open3 rdoc sqlite3 yaml ]
+  dependencies.reject! { |d| stubs.include?(d) }
 
-  # All other dependencies are included with the installation of Ruby
-  # in macOS. Adding these dependencies anyway will cause errors that
-  # prevent icalPal from being installed, so we need to be more
-  # granular.
-  if RUBY_VERSION >= '3.0'
-    dependencies.push('logger')
-    dependencies.push('csv')
-    dependencies.push('json')
-    dependencies.push('rdoc')
-    dependencies.push('sqlite3')
-    dependencies.push('yaml')
+  unless dependencies.empty?
+    di = Gem::DependencyInstaller.new
+    dependencies.each { |d| di.install(d) }
   end
-
-  di = Gem::DependencyInstaller.new(install_dir: gemdir)
-  dependencies.each { |d| di.install(d) }
 
 rescue Exception => e
   abort(e)
 end
-
-File.write('Makefile', "clean:\n\ttrue\ninstall:\n\ttrue")
 
 exit(0)
